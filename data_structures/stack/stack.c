2d18ce0ff0b168498cfc0e2d422399997533d298
jScriptural <isongjohn014@gmail.com>
2024-05-15 11:21:09 +0000

    stack implementation

---
+++ data_structures/stack/stack.c
@@ -0,0 +1,10 @@
+/*
+ * STACK.C
+ * 
+ * Author: Isonguyo John Sunday <isongjohn014@gmail.com>
+ * created on 15/05/2024
+ *
+ *
+ *
+ *
+ */
---
+++ data_structures/stack/stack.h
@@ -0,0 +1,9 @@
+/*
+ * STACK.H
+ *
+ * Author: Isonguyo John Sunday <isongjohn014@gmail.com>
+ * Created on 15/05/2024
+ *
+ */
+
+
